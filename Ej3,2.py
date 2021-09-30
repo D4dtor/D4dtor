96393e1c125c849072b7f41de43a2cf4f2e7bdfb
D4dtor <91593858+D4dtor@users.noreply.github.com>
2021-09-30 13:59:11 +0200

    Add files via upload

---
+++ Ej3,2.py
@@ -0,0 +1,12 @@
+"""3.2 Introduciendo un número en el teclado de ordenador, hallar cuantos números enteros múltiplos de tres, hay comprendidos entre la unidad y dichonúmero."""
+a=int(input("Dime un numero: "))
+x=0
+
+res=x*3
+while(res<a-2):
+    
+    x=x+1
+    res=x*3
+    
+    
+print("El numero de multiplos de 3 que habrá será o serán: ",x)
